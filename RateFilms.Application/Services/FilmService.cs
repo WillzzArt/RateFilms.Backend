07ef154c06a4a8d9aa2491725865076d348b1244
WillzzArt <ermolaew.danila2016@gmail.com>
2023-10-24 18:13:56 +0300

    test com

--- RateFilms.Application/Services/FilmService.cs
+++ RateFilms.Application/Services/FilmService.cs
@@ -23,6 +23,36 @@ namespace RateFilms.Application.Services
         public async Task CreateFilmsAsync(Film film)
         {
             await _repository.CreateAsync(film);
+            /*var actors = film.Actors;
+            
+            var genres = film.Genre;
+
+            var img = film.Images;
+
+            if (actors != null)
+            {
+
+                foreach (var actor in actors)
+                {
+                    var image = actor.Image;
+                    if (image != null)
+                        await _repository.CreateAsync(image);
+
+                    await _repository.CreateAsync(actor);
+                    
+                }
+                
+            }
+
+            if (genres != null)
+                foreach (var genre in genres)
+                    await _repository.CreateAsync(genre);
+
+            if (img != null)
+                foreach(var image in img)
+                    await _repository.CreateAsync(image);
+
+            await _repository.CreateAsync(film);*/
         }
 
         public async Task<IEnumerable<Film?>> GetFilms()
